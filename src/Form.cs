62a40a2f50fd227d91b8ad858385209d99c28ddc
L9406 <l9406@student.jamk.fi>
2021-11-11 16:54:47 +0200

    Remove unnecessary imports

--- src/Form.cs
+++ src/Form.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Globalization;
 using Microsoft.Win32;
-using System.Threading;
 
 namespace week_number_in_tray
 {
